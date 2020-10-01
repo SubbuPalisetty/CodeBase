14eed9d24ba31934fb9acd76f43d2b50623012d3
SubbuPalisetty <59964866+SubbuPalisetty@users.noreply.github.com>
2020-10-01 10:45:54 +0530

    Update Demo.java

--- src/main/java/com/person/demo/Demo.java
+++ src/main/java/com/person/demo/Demo.java
@@ -2,10 +2,10 @@ package com.person.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.person.*")
+@EnableSwagger2
 public class Demo {
 
 	public static void main(String[] args) {
